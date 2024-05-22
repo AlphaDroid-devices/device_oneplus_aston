8d45fa8ee5ee86faaf0960e64cd71bb898a61be2
lostark13 <tushar9915@gmail.com>
2024-05-22 13:50:10 +0800

    aston: parts: Initialize parts with per-app refresh rate
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2376
 TARGET_SCREEN_WIDTH := 1080
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    AstonParts
+
 # IR
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
