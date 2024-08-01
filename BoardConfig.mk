5fa50bab292d9403c54008c7e58f6d75340e2629
elpaablo <donopwhile1@gmail.com>
2024-08-01 08:43:47 +0000

    sepolicy: handle remaining denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,10 @@ TARGET_SCREEN_DENSITY := 450
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/oplus/aston.config
 
+# Kernel
+# BOARD_BOOTCONFIG += \
+#    androidboot.selinux=permissive
+
 # Kernel modules
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.system_dlkm))
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
@@ -38,6 +42,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vibrator
 SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_VIBRATOR_HAL
