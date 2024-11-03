436cf9c305adf1697e553b72541d741628f578ff
elpaablo <donopwhile1@gmail.com>
2024-11-03 02:48:09 +0000

    AlphaDroid bringup
    Change-Id: I34af68fea86ee1f92da9c0264830f0ed7e8312da

---
+++ alpha_aston.mk
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from aston device
+$(call inherit-product, device/oneplus/aston/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+PRODUCT_NAME := alpha_aston
+PRODUCT_DEVICE := aston
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2609
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi-user 15 AP3A.240617.008 1757337196356 release-keys" \
+    BuildFingerprint=OnePlus/CPH2609EEA/OP5D35L1:15/TP1A.220905.001/U.R4T3.3f1b53e-1f64387-1f81114:user/release-keys \
+    DeviceName=OP5D35L1 \
+    DeviceProduct=CPH2609 \
+    SystemDevice=OP5D35L1 \
+    SystemName=CPH2609
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+ifeq ($(TARGET_BUILD_PACKAGE),3)
+  # (valid only for GAPPS builds)
+  TARGET_INCLUDE_GOOGLE_COMMS := true
+  TARGET_INCLUDE_PIXEL_LAUNCHER := true
+  TARGET_SUPPORTS_QUICK_TAP := true
+  TARGET_SUPPORTS_CALL_RECORDING := true
+  TARGET_INCLUDE_STOCK_ARCORE := true
+  TARGET_INCLUDE_LIVE_WALLPAPERS := true
+  TARGET_SUPPORTS_GOOGLE_RECORDER := false
+endif
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := elpaablo
