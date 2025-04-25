8fad9bd1ee95ba984b8e0235fe38928fe8101bf8
elpaablo <donopwhile1@gmail.com>
2025-04-26 00:13:43 +0200

    Build dtb from source

--- alpha_aston.mk
+++ alpha_aston.mk
@@ -30,7 +30,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemDevice=OP5D35L1 \
     SystemName=CPH2585
 
-TARGET_USES_PREBUILT_DTB := true
+TARGET_USES_PREBUILT_DTB := false
 
 # Device config
 TARGET_HAS_UDFPS := true
