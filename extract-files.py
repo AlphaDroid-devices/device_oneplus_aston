281ae31bf2734e5865f2ae1b5ca883383cc729ca
inferno0230 <mail@inferno0230.in>
2025-06-15 11:53:41 +0530

    aston: Initial proprietary files listing
    Change-Id: I2a3b7e3d8f50a2fa0f687b8a5a6afcd72c04983d
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -9,6 +9,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -18,17 +19,15 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
+    'vendor/oneplus/sm8550-common',
     'hardware/oplus',
     'hardware/qcom-caf/sm8550',
-    'vendor/oneplus/sm8550-common',
     'vendor/qcom/opensource/display',
     'vendor/qcom/opensource/commonsys-intf/display',
 ]
 
-
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
-
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
@@ -43,18 +42,9 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('SystemCamera =  0;  0;  0;  1;  0;  1;', 'SystemCamera =  0;  0;  0;  0;  0;  0;'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
-    (
-        'odm/lib64/libCOppLceTonemapAPI.so',
-        'odm/lib64/libCS.so',
-        'odm/lib64/libSuperRaw.so',
-        'odm/lib64/libYTCommon.so',
-        'odm/lib64/libyuv2.so'
-    ): blob_fixup()
+    ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libSuperRaw.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libyuv2.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    (
-        'odm/lib64/libHIS.so',
-        'odm/lib64/libOGLManager.so'
-    ): blob_fixup()
+    ('odm/lib64/libHIS.so', 'odm/lib64/libOGLManager.so', 'odm/lib64/libOPAlgoCamFaceBeautyCap.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
         .clear_symbol_version('AHardwareBuffer_lock')
@@ -72,7 +62,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
-        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0')
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
@@ -81,7 +71,7 @@ module = ExtractUtilsModule(
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
-    add_firmware_proprietary_file=True,
+    add_firmware_proprietary_file=False,
 )
 
 if __name__ == '__main__':
