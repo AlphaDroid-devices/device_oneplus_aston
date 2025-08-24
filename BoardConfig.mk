9328e442a075594382e89bfd6818f50ddebbaa93
elpaablo <donopwhile1@gmail.com>
2025-08-24 20:02:12 +0100

    Add option to make permissive builds
    Change-Id: I62838fb001ba7827006de4665fa957e5664fdf39

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@ endif
 
 # Kernel
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_ASTON_DTB=y
+ifeq ($(TARGET_BUILD_PERMISSIVE),true)
+  BOARD_BOOTCONFIG += androidboot.selinux=permissive
+endif
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
