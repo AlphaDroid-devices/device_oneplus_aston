e1e452509deac421e86699712fe0fc4a7c27ebbf
elpaablo <donopwhile1@gmail.com>
2025-09-27 01:06:46 +0100

    Set dt2w sysnode via soong config
    Change-Id: I2c3f2a64140c3366ff87b7bbf90df57a5f0bc548

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,9 +33,6 @@ ifeq ($(TARGET_BUILD_PERMISSIVE),true)
   BOARD_BOOTCONFIG += androidboot.selinux=permissive
 endif
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
