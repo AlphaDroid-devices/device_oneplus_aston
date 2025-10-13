14b5b60f08e4e07ccd2cf32aea1dd58ace621be4
elpaablo <donopwhile1@gmail.com>
2025-10-13 21:25:19 +0100

    LiveDisplay: disable anti-flicker
    Change-Id: I1c55d2ee8f2b5d6585fe16813d5ec55d28541f08

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_exten
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
 # LiveDisplay
-$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
+$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,false)
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Overlays
