e5b5cccaa34bf6601006b7d60d1232a6e7190720
Pranav Vashi <neobuddy89@gmail.com>
2025-11-18 00:56:37 +0530

    aston: Include ConsumerIR prebuilt
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,8 @@ $(call soong_config_set_bool,qtidisplay,pxlw_hw_iris7,true)
 TARGET_HAS_UDFPS := true
 
 # IR
+$(call inherit-product, vendor/oneplus/ir/config.mk)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
 
